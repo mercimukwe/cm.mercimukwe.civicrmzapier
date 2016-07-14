c96872ce2e8f61afda003c5c60aebc7519a9ed17
Merci M <mercimukwe@gmail.com>
2016-07-14 23:57:43 +0100

    restructured Zapier and Survey classes

--- CRM/Civicrmzapier/Survey.php
+++ CRM/Civicrmzapier/Survey.php
@@ -1,17 +1,26 @@
 <?php
 
 /**
-* 
+*  $this -> userAuth = "api_key";
+$this -> $userAuthValue = "T3KxLDWdkJtND3P9";
+$this -> timeOut  = 30;
+
+// we set the default action to post unless otherwise specified then we reconfigure the action with the others
+
+$this -> action = "POST";
 */
-class CRM_Zapiercivicrm_Survey{
-	
-	/* tresholdAmount, email, surveyData*/
+
+class CRM_Zapiercivicrm_Survey extends CRM_Zapiercivicrm_Zapier{
 
 
-	function __construct(argument)
-	{
-		# code...
-	}
+    public static $tresholdAmount;
+    public $receiverEmail;
+    public static $surveyData;
+    public $surveyName;
+
+
+
+
 
 
 }
--- CRM/Civicrmzapier/Zapier.php
+++ CRM/Civicrmzapier/Zapier.php
@@ -8,16 +8,18 @@
 
 class CRM_Zapiercivicrm_Zapier{
 
-/*
-variables 
+    /**
+     * This class provides a generic level of allcommunications between CiviCRM and Zapier
+     *  that is sending and receiving data using the webhook
+     */
 
- hookUrl 
- userAuth
- userAuthValue 
- timeOut 
- action
+    public static $hookUrl ;
+    public static $userAuth;
+    public static $userAuthValue ;
+    public static $timeOut ;
+    public        $action ;    // this will vary based on the request
+    public static $requestContent;   // result from out api call will be placed here (json)
 
-*/
  /**
    * Method Do necessary Configurations
    *
@@ -26,10 +28,18 @@ variables
    * @access public
    * @static
    */
-	public static function config(){
 
+	public static function config($hookUrl ,$userAuth , $userAuthValue , $timeOut , $action, $requestContent){
 
-	}
+        self::setHookUrl($hookUrl);
+        self::setUserAuth($userAuth);
+        self::setUserAuthValue($userAuthValue);
+        self::setTimeOut($timeOut);
+        $this->setAction($action);
+        self::setRequestContent($requestContent);
+
+    }
+
 
 	 /**
    * Method setup the webhook Data 
@@ -40,6 +50,8 @@ variables
    * @static
    */
 	public static function setUpWebhook(){
+
+        // this function makes available the data for CURL to use for requests
 
 
 	}
@@ -54,6 +66,7 @@ variables
    */
 	public static function sendRequest(){
 
+	    // this is where the request is sent using curl
 
 	}
 
@@ -67,8 +80,60 @@ variables
    */
 	public static function receiveResponse(){
 
+	    //here we receive the response and then we could make it available for confirmation
+
+
 
 	}
+
+
+    /**
+     * @param mixed $action
+     */
+    public function setAction($action)
+    {
+        $this->action = $action;
+    }
+
+    /**
+     * @param mixed $hookUrl
+     */
+    public static function setHookUrl($hookUrl)
+    {
+        self::$hookUrl = $hookUrl;
+    }
+
+    /**
+     * @param mixed $requestContent
+     */
+    public static function setRequestContent($requestContent)
+    {
+        self::$requestContent = $requestContent;
+    }
+
+    /**
+     * @param mixed $timeOut
+     */
+    public static function setTimeOut($timeOut)
+    {
+        self::$timeOut = $timeOut;
+    }
+
+    /**
+     * @param mixed $userAuth
+     */
+    public static function setUserAuth($userAuth)
+    {
+        self::$userAuth = $userAuth;
+    }
+
+    /**
+     * @param mixed $userAuthValue
+     */
+    public static function setUserAuthValue($userAuthValue)
+    {
+        self::$userAuthValue = $userAuthValue;
+    }
 
 
 }
