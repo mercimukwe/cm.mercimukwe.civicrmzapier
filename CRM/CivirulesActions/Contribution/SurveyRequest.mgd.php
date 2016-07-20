a5b1b11caaac3504f75ec3f021dcf62efc9e58f1
Merci M <mercimukwe@gmail.com>
2016-07-20 09:26:16 +0100

    added action class and managed entity for civirules action

---
+++ CRM/CivirulesActions/Contribution/SurveyRequest.mgd.php
@@ -0,0 +1,17 @@
+<?php
+return array (
+    0 =>
+        array (
+            'name' => 'Civirules:Action.SurveyRequest',
+            'entity' => 'CiviRuleAction',
+            'params' =>
+                array (
+                    'version' => 3,
+                    'name' => 'SurveyRequest',
+                    'label' => 'Send a Survey with Zapier',
+                    'class_name' => 'CRM_CivirulesActions_Contribution_SurveyRequest',
+                    'is_active' => 1
+                ),
+        ),
+);
+
---
+++ CRM/CivirulesActions/Contribution/SurveyRequest.php
@@ -0,0 +1,15 @@
+<?php
+
+Class CRM_CivirulesActions_SurveyRequest extends CRM_Civirules_Action {
+
+    public function getExtraDataInputUrl($ruleActionId) {
+        return FALSE;
+    }
+
+
+
+    public function processAction(CRM_Civirules_TriggerData_TriggerData $triggerData){
+
+    }
+
+}
